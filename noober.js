3a4b1f6cd61fb30b56ab99b66a0e770b26d76c33
alo924 <alexandra.ochalek@kellogg.northwestern.edu>
2021-02-01 23:33:13 -0600

    update 14

--- noober.js
+++ noober.js
@@ -18,7 +18,8 @@ async function pageLoaded() {
   let outputElement = document.querySelector ('.rides')
 
     for (let i=0; i<json.length; i++){
-      
+      let ride = json
+
       if (json[i].length>1)  {
       levelofService = 'Noober Pool'
       outputElement.insertAdjacentHTML('beforeend',`
