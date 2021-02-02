a774baa2a85d30fe11facc254f32169d203cf73c
alo924 <alexandra.ochalek@kellogg.northwestern.edu>
2021-02-01 21:52:11 -0600

    update7

--- noober.js
+++ noober.js
@@ -8,21 +8,15 @@ async function pageLoaded() {
   // 🔥 start here: write code to loop through the rides
   
  for (let i=0; i<rides.length; i++){
-   let ride=rides[i]
-
-  for (let j=0; j<ride.length; j++){
-    let element=document.querySelector('.rides')
-    element.insertAdjacentHTML(`beforeend`, renderProduct(ride))
-  }
-   
+   let ride=json[i]
+    renderRide(ride)
   }
 
- let outputElement=document.querySelector('.rides')
+  function renderRide(ride){
+    let outputElement=document.querySelector('.rides')
+    outputElement.insertAdjacentHTML('beforeend',`)
 
- for (let n=0; n<ride.length; n++){
- passenger=ride[n]
-
-    if (ride.length>1) {
+    ${if (ride.length>1) {
       levelofService = 'Noober Pool'
     } else if (ride[0].purpleRequested) {
       levelofService = 'Noober Purple'
@@ -31,35 +25,16 @@ async function pageLoaded() {
     } else levelofService = 'Noober X'
   }
 
-  passenger1Name = `${ride[0].passengerDetails.first} ${ride[0].passengerDetails.last}`
-  passenger1Phone = ride[0].passengerDetails.phoneNumber
-  passenger1NumberOfPassengers = ride[0].numberOfPassengers
-  passenger1PickupAddressLine1 = ride[0].pickupLocation.address
-  passenger1PickupAddressLine2 = `${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}`
-  passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
-  passenger1DropoffAddressLine2 = `${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}`
-
-  if (ride.length > 1) {
-    passenger2Name = `${ride[1].passengerDetails.first} ${ride[1].passengerDetails.last}`
-    passenger2Phone = ride[1].passengerDetails.phoneNumber
-    passenger2NumberOfPassengers = ride[1].numberOfPassengers
-    passenger2PickupAddressLine1 = ride[1].pickupLocation.address
-    passenger2PickupAddressLine2 = `${ride[1].pickupLocation.city}, ${ride[1].pickupLocation.state} ${ride[1].pickupLocation.zip}`
-    passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
-    passenger2DropoffAddressLine2 = `${ride[1].dropoffLocation.city}, ${ride[1].dropoffLocation.state} ${ride[1].dropoffLocation.zip}`
+    <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+  <i class="fas fa-car-side"></i>
+  <span>Noober X</span>
+</h1>  
+    `)
 
   }
-  if (ride.length > 2) {
-    passenger3Name = `${ride[2].passengerDetails.first} ${ride[2].passengerDetails.last}`
-    passenger3Phone = ride[2].passengerDetails.phoneNumber
-    passenger3NumberOfPassengers = ride[2].numberOfPassengers
-    passenger3PickupAddressLine1 = ride[2].pickupLocation.address
-    passenger3PickupAddressLine2 = `${ride[2].pickupLocation.city}, ${ride[2].pickupLocation.state} ${ride[2].pickupLocation.zip}`
-    passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
-    passenger3DropoffAddressLine2 = `${ride[2].dropoffLocation.city}, ${ride[2].dropoffLocation.state} ${ride[2].dropoffLocation.zip}`
-  }
+} 
+  
 
-}
-
-// window.addEventListener('DOMContentLoaded', pageLoaded)
+  
+window.addEventListener('DOMContentLoaded', pageLoaded)
 
