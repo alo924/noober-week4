7d9fb8fd85146410b90eb47221d37636ab6eb31a
alo924 <alexandra.ochalek@kellogg.northwestern.edu>
2021-02-01 22:01:48 -0600

    update8

--- noober.js
+++ noober.js
@@ -7,33 +7,57 @@ async function pageLoaded() {
   
   // 🔥 start here: write code to loop through the rides
   
- for (let i=0; i<rides.length; i++){
-   let ride=json[i]
-    renderRide(ride)
-  }
+  for (let i=0; i<json.length; i++){
+     ride=json[i]
+     
+  let outputElement=document.querySelector('.rides')
 
-  function renderRide(ride){
-    let outputElement=document.querySelector('.rides')
-    outputElement.insertAdjacentHTML('beforeend',`)
+    for (let n=0; n<ride.length; n++){
+      passenger = ride[n]
+      if (ride.length>1) {
+        levelofService = 'Noober Pool'
+      } else if (ride[0].purpleRequested) {
+        levelofService = 'Noober Purple'
+      } else if (ride[0].numberOfPassengers>3) {
+        levelofService = 'Noober XL'
+      } else levelofService = 'Noober X'
+}
 
-    ${if (ride.length>1) {
-      levelofService = 'Noober Pool'
-    } else if (ride[0].purpleRequested) {
-      levelofService = 'Noober Purple'
-    } else if (ride[0].numberOfPassengers>3) {
-      levelofService = 'Noober XL'
-    } else levelofService = 'Noober X'
-  }
+  outputElement.insertAdjacentHTML('beforeend',`
+  <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+      <i class="fas fa-car-side"></i>
+      <span>${levelofService}</span>
+    </h1>
 
-    <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
-  <i class="fas fa-car-side"></i>
-  <span>Noober X</span>
-</h1>  
-    `)
+    <div class="border-4 border-gray-900 p-4 my-4 text-left">
+      <div class="flex">
+        <div class="w-1/2">
+          <h2 class="text-2xl py-1">Foghorn Leghorn</h2>
+          <p class="font-bold text-gray-600">(312) 555-1212</p>
+        </div>
+        <div class="w-1/2 text-right">
+          <span class="rounded-xl bg-gray-600 text-white p-2">
+            2 passengers
+          </span>
+        </div>
+      </div>
+      <div class="mt-4 flex">
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">PICKUP</div>
+          <p>123 Main St</p>
+          <p>Chicago, IL 60603</p>
+        </div>
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+          <p>123 Main St</p>
+          <p>Chicago, IL 60603</p>
+        </div>
+      </div>
+    </div>
 
-  }
-} 
-  
+
+
+ 
 
   
 window.addEventListener('DOMContentLoaded', pageLoaded)
