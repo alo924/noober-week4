139ba934bff29f649d8e71dc0153858fe841ac55
alo924 <alexandra.ochalek@kellogg.northwestern.edu>
2021-02-01 20:02:02 -0600

    update 4

--- noober.js
+++ noober.js
@@ -1,9 +1,9 @@
-async function pageLoaded() {
-  let response = await fetch('https://kiei451.com/api/rides.json')
-  let json = await response.json()
+// async function pageLoaded() {
+  // let response = await fetch('https://kiei451.com/api/rides.json')
+  // let json = await response.json()
 
   // writes the returned JSON to the console
-  console.dir(json)
+  // console.dir(json)
   
   // 🔥 start here: write code to loop through the rides
   
